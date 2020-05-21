499710779fe3e64721a729e1119d3d321037a0ca
jyota <jyota@my.ccsu.edu>
2020-05-20 22:05:23 -0700

    Basic monitor intake (without trigger intake yet)

--- Program.fs
+++ Program.fs
@@ -1,5 +1,5 @@
 open System
-open FSharp.Data
+//open FSharp.Data
 open System.Linq
 open HtmlAgilityPack.FSharp
 open FSharp.Data.Npgsql
@@ -75,6 +75,102 @@ let main argv =
     0
 *)
 
+let insertUrl (url : string) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) =
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.urls (url)
+        VALUES (@url)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(url = url)
+    t.Head
+
+let insertMonitor (urlId : int64) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) =
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.monitors (url_id, enabled)
+        VALUES (@url_id, true)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(url_id = urlId)
+    t.Head
+
+let insertDomPath (path : string) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) =
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.dom_paths (path)
+        VALUES (@path)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(path = path)
+    t.Head
+
+let insertMonitorsDomPath (monitor_id : int64) (dom_path_id : int64) (monitor_dom_target_type_id : int64) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) = 
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.monitors_dom_paths (monitor_id, dom_path_id, monitor_dom_target_type_id)
+        VALUES (@monitor_id, @dom_path_id, @monitor_dom_target_type_id)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(monitor_id = monitor_id, dom_path_id = dom_path_id, monitor_dom_target_type_id = monitor_dom_target_type_id)
+    t.Head
+
+let insertUserMonitor (monitor_id : int64) (user_id : int64) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) =
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.user_monitors (monitor_id, user_id)
+        VALUES (@monitor_id, @user_id)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(monitor_id = monitor_id, user_id = user_id)
+    t.Head
+
+let insertPriceHistory (monitor_id : int64) (price : decimal) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) =
+    use cmd = new NpgsqlCommand<"
+        INSERT INTO md.price_history (price, monitor_source_id)
+        VALUES (@price, @monitor_id)
+        RETURNING id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(price = price, monitor_id = monitor_id)
+    t.Head
+
+let getDomPathTypeId (name : string) (conn : Npgsql.NpgsqlConnection) (tx : Npgsql.NpgsqlTransaction) = 
+  use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.target_types
+        WHERE name = @name", PricingMonitorDbConnectionString>(conn, tx)
+  let t = cmd.Execute(name = name)
+  t.Head
+
+let monitorIntake (monitorRequest : MonitorRequest) (path : string) (price : decimal) =
+    use conn = new Npgsql.NpgsqlConnection(PricingMonitorDbConnectionString)
+    conn.Open()
+    use tx = conn.BeginTransaction()
+    use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.urls WHERE url = @url", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(url = monitorRequest.Url)
+    let urlId = if t.Length > 0 then t.Head else insertUrl monitorRequest.Url conn tx
+
+    use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.monitors WHERE url_id = @url_id", PricingMonitorDbConnectionString>(conn, tx)
+    let mt = cmd.Execute(url_id = urlId)
+    let monitorId = if mt.Length > 0 then mt.Head else insertMonitor urlId conn tx
+
+    // TODO: ensure an old/expired DOM path gets replaced with the newer one from intake if URL is the same(?)
+    use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.dom_paths WHERE path = @path", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(path = path)
+    let domPathId = if t.Length > 0 then t.Head else insertDomPath path conn tx
+    let priceDomPathTypeId = getDomPathTypeId "Price" conn tx
+
+    use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.monitors_dom_paths 
+        WHERE monitor_id = @monitor_id 
+        AND dom_path_id = @dom_path_id
+        AND monitor_dom_target_type_id = @target_type_id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(monitor_id = monitorId, dom_path_id = domPathId, target_type_id = priceDomPathTypeId)
+    ignore <| if t.Length = 0 then insertMonitorsDomPath monitorId domPathId priceDomPathTypeId conn tx else int64 0
+
+    use cmd = new NpgsqlCommand<"
+        SELECT id FROM md.user_monitors 
+        WHERE monitor_id = @monitor_id
+        AND user_id = @user_id", PricingMonitorDbConnectionString>(conn, tx)
+    let t = cmd.Execute(monitor_id = monitorId, user_id = monitorRequest.RequestingUserId)
+    ignore <| if t.Length > 0 then t.Head else insertUserMonitor monitorId monitorRequest.RequestingUserId conn tx
+
+    // If monitor was not here before, insert first price into it's history
+    ignore <| if mt.Length = 0 then insertPriceHistory monitorId price conn tx else int64 0
+    tx.Commit()
+    true
+
 let getMonitorRequestActions urlTargetId = 
   use cmd = PricingMonitorDb.CreateCommand<"
                   SELECT id, action_id, action_trigger_id, action_trigger_threshold, threshold_type_id, action_target_text
@@ -99,7 +195,10 @@ let handleJob (jobData : MonitorRequest) =
   async {
     let scrapedInfo = scrapePriceURL jobData.Url (string jobData.TargetPrice)
     if scrapedInfo.FoundPrice then
-      printfn "\nURL: %s\nTarget: %f\nPath: %s\nFound price %b\n" jobData.Url jobData.TargetPrice scrapedInfo.Path scrapedInfo.FoundPrice
+      if monitorIntake jobData scrapedInfo.Path jobData.TargetPrice then 
+        printfn "\nURL: %s\nTarget: %f\nPath: %s\nFound price %b\n" jobData.Url jobData.TargetPrice scrapedInfo.Path scrapedInfo.FoundPrice
+      else
+        printfn "\nUnable to intake monitor\n"
     else
       printfn "\nDidn't find price\n"
   }
